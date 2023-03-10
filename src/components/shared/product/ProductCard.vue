af84eff0099a616ba11d91e33cafef31a2d9d6a5
ropodl <sarox14@gmail.com>
2023-03-10 14:59:51 +0545

    Update ProductCard.vue

--- src/components/shared/product/ProductCard.vue
+++ src/components/shared/product/ProductCard.vue
@@ -15,51 +15,59 @@ const isDark = computed(() => {
 </script>
 <template>
   <v-hover v-slot="{ isHovering, props }">
-    <v-card
-      flat
-      :hover="false"
-      color="transparent"
-      class="h-100"
-      :to="'/product/' + product['id']"
-      :active="false"
-      v-bind="props"
-    >
-      <div class="position-relative">
-        <v-img
-          cover
-          height="200"
-          :src="product['images'][0]"
-          :class="['rounded', isHovering ? 'zoom' : '']"
-        >
-          <div class="d-flex align-center">
+    <v-responsive :aspect-ratio="16 / 9">
+      <v-card
+        border
+        flat
+        color="transparent"
+        class="h-100"
+        :to="'/product/' + product['id']"
+        :active="false"
+        v-bind="props"
+      >
+        <div class="position-relative">
+          <v-img
+            cover
+            :src="product['images'][0]"
+            :class="['rounded overflow-hidden', isHovering ? 'zoom' : '']"
+          >
+            <div class="d-flex align-center">
+              <v-spacer></v-spacer>
+              <v-btn icon flat color="transparent" @click.prevent="test">
+                <v-icon color="red-darken-2" :icon="mdiHeartOutline"></v-icon>
+              </v-btn>
+            </div>
+          </v-img>
+          <v-card-text
+            class="py-0 w-100 d-flex px-0"
+            style="position: absolute; bottom: 0"
+          >
             <v-spacer></v-spacer>
-            <v-btn icon flat color="transparent" @click.prevent="test">
-              <v-icon color="red-darken-2" :icon="mdiHeartOutline"></v-icon>
-            </v-btn>
-          </div>
-        </v-img>
+            <v-chip
+              label
+              rounded="0"
+              variant="flat"
+              color="primary"
+              class="font-weight-bold"
+              v-text="'Rs.' + product['price']"
+              style="
+                border: 4px solid rgb(var(--v-theme-background));
+                border-bottom: 0;
+                border-right: 0;
+              "
+            ></v-chip>
+          </v-card-text>
+        </div>
         <v-card-text
-          class="py-0 w-100 d-flex px-0"
-          style="position: absolute; bottom: 0"
-        >
-          <v-spacer></v-spacer>
-          <v-chip
-            label
-            rounded="0"
-            variant="flat"
-            color="primary"
-            class="font-weight-bold"
-            v-text="'Rs.' + product['price']"
-            style="border: 4px solid rgb(var(--v-theme-background))"
-          ></v-chip>
-        </v-card-text>
-      </div>
-      <v-card-text
-        class="font-weight-bold pb-0"
-        v-text="product['title']"
-      ></v-card-text>
-      <v-card-subtitle class="pb-3">Nrs. 725</v-card-subtitle>
-    </v-card>
+          class="font-weight-bold pb-0"
+          v-text="product['title']"
+        ></v-card-text>
+        <v-card-subtitle class="font-weight-bold pb-3">
+          <span class="text-primary mr-2">Nrs. 725</span>
+          <span class="text-decoration-line-through">test</span>
+        </v-card-subtitle>
+      </v-card>
+    </v-responsive>
   </v-hover>
 </template>
 <style lang="scss">
@@ -75,13 +83,5 @@ const isDark = computed(() => {
       transform: scale(1.1);
     }
   }
-}
-div.v-overlay__scrim.bg-gradiant {
-  opacity: 1;
-  background: linear-gradient(
-    0deg,
-    rgba(var(--v-theme-surface), 1) 20%,
-    rgba(0, 212, 255, 0) 100%
-  ) !important;
 }
 </style>
